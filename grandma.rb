3e40105867928905311efcb4e6da25ad45200bf2
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-06 00:22:11 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,65 @@
 # Write a speak_to_grandma method.
 
+#def speak_to_grandma(phrase)
+ # #phrase == "I Love You Grandma!"
+  #if phrase != phrase.upcase
+   # return "HUH?! SPEAK UP, SONNY!"
+  #elsif phrase.upcase
+   # return "NO, NOT SINCE 1938!"
+  #else phrase(phrase)
+  #return "LOVE YOU TOO PUMKIN!"
+  #end
+#end
+  
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  #elsif phrase == phrase.downcase
+   # return "HUH?! SPEAK UP, SONNY!"
+  else phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+ # else 
+   # return "NO, NOT SINCE 1938!"
+  end
+end 
+ 
+
+   
+   
+ 
+ 
+ 
+ 
+ # grandson == "I LOVE YOU GRANDMA!"
+  #if grandson == "I LOVE YOU GRANDMA!"
+   # puts "I LOVE YOU TOO PUMPKIN!"
+  #elsif grandson == "Hi Nana, how are you?"
+   # puts "HUH?! SPEAK UP, SONNY!"
+  #elsif grandson == "Hi!"
+   # puts "HUH?! SPEAK UP, SONNY!"
+  #else grandson == "WHAT DID YOU EAT TODAY?"
+   # puts "NO, NOT SINCE 1938!"
+   
+  
+  
+
+   #grandma == "I LOVE YOU GRANDMA!"
+  #if grandma == "I LOVE YOU GRANDMA!"
+   # puts "I LOVE YOU TOO PUMPKIN!"
+  #elsif grandma == "Hi nana, how are you?"
+   # puts "HUH?! SPEAK UP, SONNY!"
+  #elsif grandma == "Hi!"
+   # puts "HUH?! SPEAK UP, SONNY!"
+  #else grandma == "WHAT DID YOU EAT TODAY?"
+   # puts "NO, NOT SINCE 1938!"
+ # else grandma == "I LOVE YOU GRANDMA!"
+  #  puts "I LOVE YOU TOO PUMPKIN!"
+    
+  
+  
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
